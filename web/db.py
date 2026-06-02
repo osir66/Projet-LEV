1e99b994056242f7c43c883d1a3a65592345fd98
enzofaure013-blip <enzofaure013@gmail.com>
2026-06-02 16:45:29 +0200

    ajout des routes des robots

--- web/db.py
+++ web/db.py
@@ -3,7 +3,7 @@ import sqlite3
 from sqlite3 import Error
 
 def ajouter_semaphore(id, etat, dessin_forme, matrice):
-    conn = sqlite3.connect("../Massilia.db")
+    conn = sqlite3.connect("Massilia.db")
     print(id, etat, dessin_forme, matrice)
     try :
         c = conn.cursor()
@@ -15,7 +15,7 @@ def ajouter_semaphore(id, etat, dessin_forme, matrice):
     conn.close()
 
 def afficher_semaphore():
-    conn = sqlite3.connect("../Massilia.db")
+    conn = sqlite3.connect("Massilia.db")
     c = conn.cursor()
     try :
         c.execute('''SELECT * FROM SEMAPHORE''')
@@ -23,5 +23,54 @@ def afficher_semaphore():
         return afficher_semaphore
     except Error as e:
         print(f"Error: {e}")
+        conn.close()
+
+def supprimer_semaphore(id):
+    conn = sqlite3.connect("Massilia.db")
+    c = conn.cursor()
+    try :
+        c.execute('''DELETE FROM SEMAPHORE WHERE id = ?''', (id,))
+        conn.commit()
+        print("Semaphore avec l'id",id, "a été supprimé.")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        conn.close()
+
+def modifier_semaphore(id, etat, dessin_forme, matrice):
+    conn = sqlite3.connect("Massilia.db")
+    c = conn.cursor()
+    try :
+        c.execute('''UPDATE SEMAPHORE SET etat = ?, dessin_forme = ?, matrice = ? WHERE id = ?''',
+              (etat, dessin_forme, matrice, id))
+        conn.commit()
+        print("Semaphore avec l'id",id, "a été modifié.")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        conn.close()    
+
+def ajouter_robot(id, position_actuelle_x, position_actuelle_y, est_disponible, vitesse_deplacement):
+    conn = sqlite3.connect("Massilia.db")
+    print(id, position_actuelle_x, position_actuelle_y, est_disponible, vitesse_deplacement)
+    try :
+        c = conn.cursor()
+        c.execute('''INSERT INTO ROBOT (id, position_actuelle_x, position_actuelle_y, est_disponible, vitesse_deplacement) VALUES (?, ?, ?, ?, ?)''',
+              (id, position_actuelle_x, position_actuelle_y, est_disponible, vitesse_deplacement))
+    except Error as e:
+        print(f"Error: {e}")
+    conn.commit()
+    conn.close()
+
+def supprimer_robot(id):
+    conn = sqlite3.connect("Massilia.db")
+    c = conn.cursor()
+    try :
+        c.execute('''DELETE FROM ROBOT WHERE id = ?''', (id,))
+        conn.commit()
+        print("Robot avec l'id",id, "a été supprimé.")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
         conn.close()
         
--- web/main.py
+++ web/main.py
@@ -11,4 +11,18 @@ def afficher_semaphore():
 def ajouter_semaphore(id: str, etat: bool, dessin_forme: str, matrice: float):
     return db.ajouter_semaphore(id, etat, dessin_forme, matrice)
 
+@app.post("/supprimer_semaphore")
+def supprimer_semaphore(id: str):
+    return db.supprimer_semaphore(id)
 
+@app.post("/modifier_semaphore")
+def modifier_semaphore(id : str, etat: bool, dessin_forme: str, matrice: float):
+    return db.modifier_semaphore(id,etat, dessin_forme, matrice)
+
+@app.post("/ajouter_robot")
+def ajouter_robot(id: str, position_actuelle_x: float, position_actuelle_y: float, est_disponible: bool, vitesse_deplacement: float):
+    return db.ajouter_robot(id, position_actuelle_x, position_actuelle_y, est_disponible, vitesse_deplacement)
+
+@app.post("/suprimer_robot")
+def suprimer_robot(id : str):
+    return db.supprimer_robot(id) 
