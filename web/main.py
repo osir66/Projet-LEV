658521c93dfe4c135a372d12dec0ff2701465c4f
enzofaure013-blip <enzofaure013@gmail.com>
2026-06-18 10:02:35 +0200

    p

--- web/main.py
+++ web/main.py
@@ -180,10 +180,8 @@ async def import_shape_csv(file: UploadFile = File(...)):
     fichier_contenu = await file.read()
     c.write_bytes(fichier_contenu)
     
-    resultat = db.import_csv(str(c))
-    if resultat is None:
-        return "fichier introuvable "
-    return resultat
+    db.import_csv(str(c))
+    return RedirectResponse(url="/commande", status_code=303)
 
 #-----------------------------------------------------------------------------------
 
